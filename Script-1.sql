e18303c8039833912f9b23c33e7ed60f0bf2f06f
Julia <juliaborgess18@gmail.com>
2022-10-17 17:42:51 -0300

    Commit inicial

--- Script-1.sql
+++ Script-1.sql
@@ -1,52 +1,52 @@
-create table empregado (
+CREATE TABLE empregado (
 	matricula integer not null,
 	salario numeric(9,2) not null,
 	descontos numeric(9,2) not null,
 	
-	constraint pk_empregado
-		primary key(matricula), --tornando matricula uma chave primária
-	constraint ck_desconto_menor_salario
-		check (descontos < salario) --utilizando check para conferir que nenhum valor de descontos será maior que salário
+	CONSTRAINT pk_empregado
+		PRIMARY KEY(matricula), --tornando matricula uma chave primária
+	CONSTRAINT ck_desconto_menor_salario
+		CHECK (descontos < salario) --utilizando CHECK para conferir que nenhum valor de descontos será maior que salário
 	)
 	
-create table empregado2(
+CREATE TABLE empregado2(
 	matricula integer not null,
 	nome varchar(40) not null,
 	cpf char(11) null,
-	constraint pk_empregado2
-		primary key(matricula),
-	constraint un_empregado_cpf
-		unique(cpf) -- definindo cpf como unique, poís é um campo candidato a chave primária, porém, pode haver campos sem cpf
+	CONSTRAINT pk_empregado2
+		PRIMARY KEY(matricula),
+	CONSTRAINT un_empregado_cpf
+		UNIQUE(cpf) -- definindo cpf como UNIQUE, poís é um campo candidato a chave primária, porém, pode haver campos sem cpf
 )
 
-create table departamento(
+CREATE TABLE departamento(
 	cod_depto integer not null,
 	nome_depto varchar(40) not null
 	default 'Departamento Novo', -- definindo que cada nova inserção na tabela que não possuir valor setado em 'nome_depto', vai receber como padrão o valor 'Departamento Novo'
-	constraint pk_departamento
-		primary key (cod_depto)
+	CONSTRAINT pk_departamento
+		PRIMARY KEY (cod_depto)
 )
 
-create table empregado3(
+CREATE TABLE empregado3(
 	matricula integer not null,
 	nome varchar(40) not null,
 	cod_dep integer not null,
-	constraint pk_empregado3
-		primary key(matricula),
-	constraint fk_empregado_departamento
-		foreign key(cod_dep) --criando uma chave estrangeira que referencia a chave primária da tabela 'departamento'
-		references departamento(cod_depto)
+	CONSTRAINT pk_empregado3
+		PRIMARY KEY(matricula),
+	CONSTRAINT fk_empregado_departamento
+		FOREIGN KEY(cod_dep) --criando uma chave estrangeira que referencia a chave primária da tabela 'departamento'
+		REFERENCES departamento(cod_depto)
 )
 
-create table empregado4 (
+CREATE TABLE empregado4 (
 	matricula integer not null,
 	nome varchar(40) not null,
 	cod_depto integer not null,
-	constraint pk_empregado4
-		primary key(matricula),
-	constraint fk_empregado_departamento4
-		foreign key(cod_depto) references departamento(cod_depto)
-		on delete cascade --quando houver remoção de um 'departamento', também os empregados serão removidos da tabela 'empregado4'
+	CONSTRAINT pk_empregado4
+		PRIMARY KEY(matricula),
+	CONSTRAINT fk_empregado_departamento4
+		FOREIGN KEY(cod_depto) REFERENCES departamento(cod_depto)
+		ON DELETE CASCADE --quando houver remoção de um 'departamento', também os empregados serão removidos da tabela 'empregado4'
 )
 
 
